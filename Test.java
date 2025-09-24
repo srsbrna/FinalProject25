8698daee62b84bb2ffb93639c499893d7095a9f3
srsbrna <srsbkusumadewi@gmail.com>
2025-09-24 10:49:12 +0700

    nextstep

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hi, ini baris kode pertama");
         System.out.println("hi, ini baris kode kedua");
+        System.out.println("hai lingga, semangat quiz bing nya ya");
     }
 }
